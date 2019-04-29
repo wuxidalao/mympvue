6d3c176552de3dea733a558eece9b0a9d3d873d0
wuxidalao <yatouxixi520@qq.com>
2019-04-29 15:59:27 +0800

    1

--- src/pages/me/index.vue
+++ src/pages/me/index.vue
@@ -1,19 +1,45 @@
 <template>
   <div>
-    <p>comments12222555</p>
+    <div class="container">
+      <div v-if="userInfo.nickName">
+        <img :src="userInfo.avatarUrl">
+        <text>{{userInfo.nickName}}</text>
+      </div>
+      <div v-if="!userInfo.nickName">
+        <img src="http://image.shengxinjing.cn/rate/unlogin.png">
+        <button open-type="getUserInfo" @getuserinfo="onGetUserinfo">登录</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {},
+  data() {
+    return {
+      userInfo: wx.getStorageSync("userInfo") || {}
+    };
+  },
 
-  components: {},
-
-  methods: {},
+  methods: {
+    onGetUserinfo(e) {
+      var userInfo = e.mp.detail.userInfo;
+      // 调用云函数
+      wx.cloud.callFunction({
+        name: "login",
+        complete: res => {
+          userInfo.openid = res.result.openid;
+          this.userInfo = userInfo;
+          // 本地存储
+          wx.setStorageSync("userInfo", userInfo);
+        }
+      });
+      console.log(e);
+    }
+  },
 
   created() {}
-}
+};
 </script>
 
 <style scoped>
