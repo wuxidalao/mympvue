417cbec7501a78badb84e06ac89a360d38d4781e
wuxidalao <yatouxixi520@qq.com>
2019-04-30 14:54:52 +0800

    1

--- cloundFn/douban/index.js
+++ cloundFn/douban/index.js
@@ -1,10 +1,21 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+const axios = require('axios')
+const doubanbook = require('doubanbook')
 
 cloud.init()
 
-getDouban(isban) {
-  const url = "https://book.douban.com/subject/" + isban
+async function getDouban(isban) {
+  const url = "https://book.douban.com/subject_search?search_text=" + isban
+  let searchInfo = await axios.get(url)
+  console.log(searchInfo.data)
+  getDouban("9787536692930")
+  let reg = /window\.__DATA__ ="(.*)"/
+  if (reg.test(searchInfo.data)) {
+    //   数据解密
+    let searchData = doubanbook(RegExp.$1)
+    console.log(searchData)
+  }
 }
 
 // 云函数入口函数
